e47be3a7eed2c3da9672892d57154fe6fcdba0cc
knev <knev@users.noreply.github.com>
2023-01-20 22:01:29 +0100

    feat: initial project files.

---
+++ example-objc-CLI-minimum/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  example-objc-CLI-minimum
+//
+//  Created by dev on 2023-01-30.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+	return 0;
+}

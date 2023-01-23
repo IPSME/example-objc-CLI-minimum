5221a7239bbbae505b3eadd70cf8bb8cfb828ac3
knev <knev@users.noreply.github.com>
2023-01-23 09:08:39 +0100

    feat: publish and subscribe.

--- example-objc-CLI-minimum/main.m
+++ example-objc-CLI-minimum/main.m
@@ -7,10 +7,55 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-	@autoreleasepool {
-	    // insert code here...
-	    NSLog(@"Hello, World!");
+#import "IPSME_MsgEnv.h"
+
+bool ipsme_handler_NSString_(id id_msg, NSString* nsstr_msg)
+{
+	NSLog(@"ipsme_handler_NSString_: [%@]", nsstr_msg);
+
+	if ([nsstr_msg isEqual:@"Discovery"]) {
+		[IPSME_MsgEnv publish:@"Announcement!"];
+		return true;
+	}
+	
+	return false;
+}
+
+void ipsme_handler_(id id_msg, NSString*)
+{
+	@try {
+		if ([id_msg isKindOfClass:[NSString class]] && ipsme_handler_NSString_(id_msg, id_msg))
+			return;
+	}
+	@catch (id ue) {
+		if ([ue isKindOfClass:[NSException class]]) {
+			NSException* e= ue;
+			NSLog(@"ERR: error is message execution: %@", [e reason]);
+		}
+		else
+			NSLog(@"ERR: error is message execution");
+		
+		return;
+	}
+	
+	NSLog(@"handler_: DROP! %@", [id_msg class]);
+}
+
+int main(int argc, const char * argv[])
+{
+	@autoreleasepool
+	{
+		[IPSME_MsgEnv subscribe:ipsme_handler_];
+
+
+		while (true)
+		{
+			[[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:1]];
+			
+			// ...
+		}
+		
 	}
 	return 0;
 }
+
